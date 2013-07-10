0a95b20d9a8763cdf6e629887bdcc51b472728e0
Ed Sweeney <ed@onextent.com>
2013-07-10 12:16:46 -0700

    refactor loc helper

---
+++ OeMap/src/main/java/com/onextent/oemap/LocationHelper.java
@@ -0,0 +1,360 @@
+package com.onextent.oemap;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationHelper implements
+        LocationListener,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener
+{
+    // Handle to SharedPreferences for this app
+    SharedPreferences mPrefs;
+    OeMapActivity context;
+
+    // Handle to a SharedPreferences editor
+    SharedPreferences.Editor mEditor;
+    // A request to connect to Location Services
+    private LocationRequest mLocationRequest;
+
+    /*
+     * Note if updates have been turned on. Starts out as "false"; is set to "true" in the
+     * method handleRequestSuccess of LocationUpdateReceiver.
+     *
+     */
+    boolean mUpdatesRequested = false;
+    // Stores the current instantiation of the location client in this object
+    private LocationClient mLocationClient;
+
+    private boolean mSharingLoc;
+
+    LocationHelper(OeMapActivity context) {
+        this.context = context;
+    }
+
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        //location
+
+        // Get handles to the UI view objects
+        //mLatLng = (TextView) findViewById(R.id.lat_lng);
+        //mAddress = (TextView) findViewById(R.id.address);
+        //mActivityIndicator = (ProgressBar) findViewById(R.id.address_progress);
+        //mConnectionState = (TextView) findViewById(R.id.text_connection_state);
+        //mConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
+
+        // Create a new global location parameters object
+        mLocationRequest = LocationRequest.create();
+
+        /*
+         * Set the update interval
+         */
+        mLocationRequest.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Set the interval ceiling to one minute
+        mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+
+        // Note that location updates are off until the user turns them on
+        mUpdatesRequested = false;
+
+        // Open Shared Preferences
+        mPrefs = context.getSharedPreferences(LocationUtils.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+
+        // Get an editor
+        mEditor = mPrefs.edit();
+
+        /*
+         * Create a new location client, using the enclosing class to
+         * handle callbacks.
+         */
+        mLocationClient = new LocationClient(context, this, this);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+            //...
+            case LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+                switch (resultCode) {
+                    case Activity.RESULT_OK :
+                    /*
+                     * Try the request again
+                     */
+                    //...
+                    break;
+                }
+            //...
+        }
+     }
+
+
+    ///////////////////////////////
+    // begin location aware impl //
+    ///////////////////////////////
+
+
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.
+                        isGooglePlayServicesAvailable(context);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                    "Google Play services is available.");
+            // Continue
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+            // Get the error code
+            //int errorCode = connectionResult.getErrorCode();
+            int errorCode = resultCode; //ejs???
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    errorCode,
+                    context,
+                    LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment =
+                        new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(context.getFragmentManager(), "Location Updates");
+                //ejs todo set content pane?
+            }
+        }
+        return false;
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
+        startUpdates(); //ejs
+    }
+
+    @Override
+    public void onDisconnected() {
+        Toast.makeText(context, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        context,
+                        LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+        public void getLocation(View v) {
+
+        // If Google Play Services is available
+        if (servicesConnected()) {
+
+            // Get the current location
+            Location currentLocation = mLocationClient.getLastLocation();
+
+            // Display the current location in the UI
+            //mLatLng.setText(LocationUtils.getLatLng(this, currentLocation));
+        }
+    }
+
+        private void showErrorDialog(int errorCode) {
+
+        // Get the error dialog from Google Play services
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+            errorCode,
+            context,
+            LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+        // If Google Play services can provide an error dialog
+        if (errorDialog != null) {
+
+            // Create a new DialogFragment in which to show the error dialog
+            ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+
+            // Set the dialog in the DialogFragment
+            errorFragment.setDialog(errorDialog);
+
+            // Show the error dialog in the DialogFragment
+            errorFragment.show(context.getFragmentManager(), "ejs");
+            //ejs todo set content pane?
+        }
+    }
+
+    /*
+     * Called when the Activity is no longer visible at all.
+     * Stop updates and disconnect.
+     */
+    public void onStop() {
+
+        // If the client is connected
+        if (mLocationClient.isConnected()) {
+            stopPeriodicUpdates();
+        }
+
+        // After disconnect() is called, the client is considered "dead".
+        mLocationClient.disconnect();
+    }
+    /*
+     * Called when the Activity is going into the background.
+     * Parts of the UI may be visible, but the Activity is inactive.
+     */
+    public void onPause() {
+
+        // Save the current setting for updates
+        //mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, mUpdatesRequested);
+        //mEditor.commit();
+    }
+
+    /*
+     * Called when the Activity is restarted, even before it becomes visible.
+     */
+    public void onStart() {
+
+        if (mLocationClient == null) throw new NullPointerException("no loc client");
+        /*
+         * Connect the client. Don't re-start any requests here;
+         * instead, wait for onResume()
+         */
+        mLocationClient.connect();
+    }
+
+    public void onResume() {
+
+        // If the app already has a setting for getting location updates, get it
+        if (mPrefs.contains(LocationUtils.KEY_UPDATES_REQUESTED)) {
+            mUpdatesRequested = mPrefs.getBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
+
+        // Otherwise, turn off location updates until requested
+        } else {
+            mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
+            mEditor.commit();
+        }
+    }
+
+    private Location mCurrLoc;
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mCurrLoc = location;
+        context.onLocationChanged(location);
+    }
+
+    private void startUpdates() {
+    //public void startUpdates(View v) {
+        mUpdatesRequested = true;
+
+        if (servicesConnected()) {
+            startPeriodicUpdates();
+        }
+    }
+
+        /**
+     * In response to a request to start updates, send a request
+     * to Location Services
+     */
+    private void startPeriodicUpdates() {
+
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+        //mConnectionState.setText(R.string.location_requested);
+    }
+
+    /**
+     * In response to a request to stop updates, send a request to
+     * Location Services
+     */
+    private void stopPeriodicUpdates() {
+        mLocationClient.removeLocationUpdates(this);
+        //mConnectionState.setText(R.string.location_updates_stopped);
+    }
+}
+
--- OeMap/src/main/java/com/onextent/oemap/OeMapActivity.java
+++ OeMap/src/main/java/com/onextent/oemap/OeMapActivity.java
@@ -1,16 +1,12 @@
 package com.onextent.oemap;
 
-import android.app.Dialog;
-import android.app.DialogFragment;
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.location.Location;
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
@@ -23,48 +19,23 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class OeMapActivity extends Activity implements
-        LocationListener,
-        GooglePlayServicesClient.ConnectionCallbacks,
-        GooglePlayServicesClient.OnConnectionFailedListener
+public class OeMapActivity extends Activity implements LocationListener
 {
-    // Handle to SharedPreferences for this app
-    SharedPreferences mPrefs;
+    LocationHelper mLocHelper;
 
-    // Handle to a SharedPreferences editor
-    SharedPreferences.Editor mEditor;
-    // A request to connect to Location Services
-    private LocationRequest mLocationRequest;
     private static final String MAPFRAGTAG = "oemap";
     private static final String SETFRAGTAG = "oeset";
-
-    /*
-     * Note if updates have been turned on. Starts out as "false"; is set to "true" in the
-     * method handleRequestSuccess of LocationUpdateReceiver.
-     *
-     */
-    boolean mUpdatesRequested = false;
-    // Stores the current instantiation of the location client in this object
-    private LocationClient mLocationClient;
 
     private boolean mSharingLoc;
     private DrawerLayout mDrawerLayout;
@@ -109,9 +80,9 @@ public class OeMapActivity extends Activity implements
 
     private GoogleMap getMap() {
 
-        if (mMapFragTag != null) {
-            FragmentManager fragmentManager = getFragmentManager();
-            MapFragment fragment = (MapFragment) fragmentManager.findFragmentByTag(mMapFragTag);
+        FragmentManager fragmentManager = getFragmentManager();
+        MapFragment fragment = (MapFragment) fragmentManager.findFragmentByTag(mMapFragTag);
+        if (fragment != null && mMapFragTag != null) {
             return fragment.getMap();
         } else {
             Log.w("ejs", "null map tag!!!");
@@ -236,6 +207,9 @@ public class OeMapActivity extends Activity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.oe_map_activity);
 
+        mLocHelper = new LocationHelper(this);
+        mLocHelper.onCreate(savedInstanceState);
+
         mPlanetTitles = getResources().getStringArray(R.array.menu_names_array);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         mTitle =  getResources().getString(R.string.app_name);
@@ -276,45 +250,6 @@ public class OeMapActivity extends Activity implements
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
-
-        //location
-
-        // Get handles to the UI view objects
-        //mLatLng = (TextView) findViewById(R.id.lat_lng);
-        //mAddress = (TextView) findViewById(R.id.address);
-        //mActivityIndicator = (ProgressBar) findViewById(R.id.address_progress);
-        //mConnectionState = (TextView) findViewById(R.id.text_connection_state);
-        //mConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
-
-        // Create a new global location parameters object
-        mLocationRequest = LocationRequest.create();
-
-        /*
-         * Set the update interval
-         */
-        mLocationRequest.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
-
-        // Use high accuracy
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        // Set the interval ceiling to one minute
-        mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
-
-        // Note that location updates are off until the user turns them on
-        mUpdatesRequested = false;
-
-        // Open Shared Preferences
-        mPrefs = getSharedPreferences(LocationUtils.SHARED_PREFERENCES, Context.MODE_PRIVATE);
-
-        // Get an editor
-        mEditor = mPrefs.edit();
-
-        /*
-         * Create a new location client, using the enclosing class to
-         * handle callbacks.
-         */
-        mLocationClient = new LocationClient(this, this, this);
-
     }
 
     /* Called whenever we call invalidateOptionsMenu() */
@@ -355,161 +290,8 @@ public class OeMapActivity extends Activity implements
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        // Decide what to do based on the original request code
-        switch (requestCode) {
-            //...
-            case LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST :
-            /*
-             * If the result code is Activity.RESULT_OK, try
-             * to connect again
-             */
-                switch (resultCode) {
-                    case Activity.RESULT_OK :
-                    /*
-                     * Try the request again
-                     */
-                    //...
-                    break;
-                }
-            //...
-        }
+        mLocHelper.onActivityResult(requestCode, resultCode, data);
      }
-
-
-    ///////////////////////////////
-    // begin location aware impl //
-    ///////////////////////////////
-
-
-    private boolean servicesConnected() {
-        // Check that Google Play services is available
-        int resultCode =
-                GooglePlayServicesUtil.
-                        isGooglePlayServicesAvailable(this);
-        // If Google Play services is available
-        if (ConnectionResult.SUCCESS == resultCode) {
-            // In debug mode, log the status
-            Log.d("Location Updates",
-                    "Google Play services is available.");
-            // Continue
-            return true;
-        // Google Play services was not available for some reason
-        } else {
-            // Get the error code
-            //int errorCode = connectionResult.getErrorCode();
-            int errorCode = resultCode; //ejs???
-            // Get the error dialog from Google Play services
-            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-                    errorCode,
-                    this,
-                    LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
-
-            // If Google Play services can provide an error dialog
-            if (errorDialog != null) {
-                // Create a new DialogFragment for the error dialog
-                ErrorDialogFragment errorFragment =
-                        new ErrorDialogFragment();
-                // Set the dialog in the DialogFragment
-                errorFragment.setDialog(errorDialog);
-                // Show the error dialog in the DialogFragment
-                errorFragment.show(getFragmentManager(), "Location Updates");
-                //ejs todo set content pane?
-            }
-        }
-        return false;
-
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-        startUpdates(); //ejs
-    }
-
-    @Override
-    public void onDisconnected() {
-        Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(
-                        this,
-                        LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
-                /*
-                 * Thrown if Google Play services canceled the original
-                 * PendingIntent
-                 */
-            } catch (IntentSender.SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
-            }
-        } else {
-            /*
-             * If no resolution is available, display a dialog to the
-             * user with the error.
-             */
-            showErrorDialog(connectionResult.getErrorCode());
-        }
-    }
-
-    // Define a DialogFragment that displays the error dialog
-    public static class ErrorDialogFragment extends DialogFragment {
-        // Global field to contain the error dialog
-        private Dialog mDialog;
-        // Default constructor. Sets the dialog field to null
-        public ErrorDialogFragment() {
-            super();
-            mDialog = null;
-        }
-        // Set the dialog to display
-        public void setDialog(Dialog dialog) {
-            mDialog = dialog;
-        }
-        // Return a Dialog to the DialogFragment.
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            return mDialog;
-        }
-    }
-        public void getLocation(View v) {
-
-        // If Google Play Services is available
-        if (servicesConnected()) {
-
-            // Get the current location
-            Location currentLocation = mLocationClient.getLastLocation();
-
-            // Display the current location in the UI
-            //mLatLng.setText(LocationUtils.getLatLng(this, currentLocation));
-        }
-    }
-
-        private void showErrorDialog(int errorCode) {
-
-        // Get the error dialog from Google Play services
-        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-            errorCode,
-            this,
-            LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
-
-        // If Google Play services can provide an error dialog
-        if (errorDialog != null) {
-
-            // Create a new DialogFragment in which to show the error dialog
-            ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-
-            // Set the dialog in the DialogFragment
-            errorFragment.setDialog(errorDialog);
-
-            // Show the error dialog in the DialogFragment
-            errorFragment.show(getFragmentManager(), "ejs");
-            //ejs todo set content pane?
-        }
-    }
 
     /*
      * Called when the Activity is no longer visible at all.
@@ -518,13 +300,7 @@ public class OeMapActivity extends Activity implements
     @Override
     public void onStop() {
 
-        // If the client is connected
-        if (mLocationClient.isConnected()) {
-            stopPeriodicUpdates();
-        }
-
-        // After disconnect() is called, the client is considered "dead".
-        mLocationClient.disconnect();
+        mLocHelper.onStop();
 
         super.onStop();
     }
@@ -535,6 +311,7 @@ public class OeMapActivity extends Activity implements
     @Override
     public void onPause() {
 
+        mLocHelper.onPause();
         // Save the current setting for updates
         //mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, mUpdatesRequested);
         //mEditor.commit();
@@ -549,31 +326,17 @@ public class OeMapActivity extends Activity implements
     public void onStart() {
 
         super.onStart();
-
-        /*
-         * Connect the client. Don't re-start any requests here;
-         * instead, wait for onResume()
-         */
-        mLocationClient.connect();
+        mLocHelper.onStart();
     }
 
         @Override
     public void onResume() {
         super.onResume();
-
-        // If the app already has a setting for getting location updates, get it
-        if (mPrefs.contains(LocationUtils.KEY_UPDATES_REQUESTED)) {
-            mUpdatesRequested = mPrefs.getBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
-
-        // Otherwise, turn off location updates until requested
-        } else {
-            mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
-            mEditor.commit();
-        }
+        mLocHelper.onResume();
     }
 
     private Location mCurrLoc;
-    @Override
+
     public void onLocationChanged(Location location) {
 
         mCurrLoc = location;
@@ -594,34 +357,6 @@ public class OeMapActivity extends Activity implements
         LatLng latLng = new LatLng(mCurrLoc.getLatitude(), mCurrLoc.getLongitude());
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
         //map.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromResource(R.drawable.common_signin_btn_icon_dark)));
-    }
-
-    public void startUpdates() {
-    //public void startUpdates(View v) {
-        mUpdatesRequested = true;
-
-        if (servicesConnected()) {
-            startPeriodicUpdates();
-        }
-    }
-
-        /**
-     * In response to a request to start updates, send a request
-     * to Location Services
-     */
-    private void startPeriodicUpdates() {
-
-        mLocationClient.requestLocationUpdates(mLocationRequest, this);
-        //mConnectionState.setText(R.string.location_requested);
-    }
-
-    /**
-     * In response to a request to stop updates, send a request to
-     * Location Services
-     */
-    private void stopPeriodicUpdates() {
-        mLocationClient.removeLocationUpdates(this);
-        //mConnectionState.setText(R.string.location_updates_stopped);
     }
 }
 
