9c33b1e53ef5280b68a6ca5ecdaf523dbedb5e81
Ed Sweeney <ed@onextent.com>
2013-07-28 12:38:15 -0700

    junit tests

--- android/OeMap/src/instrumentTest/java/com/onextent/test/oemap/PresenceTestCase.java
+++ android/OeMap/src/instrumentTest/java/com/onextent/test/oemap/PresenceTestCase.java
@@ -8,15 +8,28 @@ import junit.framework.TestCase;
 
 public class PresenceTestCase extends TestCase {
 
+    static final double LATITUDE  = 30.1;
+    static final double LONGITUDE = 40.2;
+    static final String PID       = "my presence id 123";
+    static final String MAPNAME   = "my map";
+    static final String LABEL     = "my name";
+    static final String SNIPPIT   = "my snippit";
+    static final String SPACENAME = "my space";
+
     protected Presence presence1;
     protected double fValue2;
 
     protected void setUp() {
-       presence1 = PresenceFactory.createPresence("123", new LatLng(0,0), "my name", "my snippet", "mymap");
+       presence1 = PresenceFactory.createPresence(PID, new LatLng(LATITUDE,LONGITUDE), LABEL, SNIPPIT, SPACENAME);
     }
 
     public void testP1() {
-       assertEquals("my name", presence1.getLabel());
+        assertEquals(PID, presence1.getPID());
+        assertEquals(LABEL, presence1.getLabel());
+        assertEquals(SNIPPIT, presence1.getSnippet());
+        assertEquals(SPACENAME, presence1.getSpaceName());
+        assertEquals(new LatLng(LATITUDE,LONGITUDE), presence1.getLocation());
+        assertFalse((new LatLng(LONGITUDE, LATITUDE)).equals(presence1.getLocation()));
     }
 }
 
