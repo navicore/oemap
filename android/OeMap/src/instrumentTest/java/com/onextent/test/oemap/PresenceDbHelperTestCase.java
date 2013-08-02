55671c3bd77161987c79610a5e60ec8e07d311b4
Ed Sweeney <ed@onextent.com>
2013-08-02 10:07:26 -0700

    presence db helper done

---
+++ android/OeMap/src/instrumentTest/java/com/onextent/test/oemap/PresenceDbHelperTestCase.java
@@ -0,0 +1,113 @@
+package com.onextent.test.oemap;
+
+import android.database.SQLException;
+import android.test.AndroidTestCase;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.onextent.oemap.presence.Presence;
+import com.onextent.oemap.presence.PresenceFactory;
+import com.onextent.oemap.presence.PresenceDbHelper;
+
+import org.json.JSONException;
+
+import java.util.List;
+
+public class PresenceDbHelperTestCase extends AndroidTestCase {
+
+    private PresenceDbHelper _dbHelper;
+
+    protected void setUp() {
+        _dbHelper = new PresenceDbHelper(getContext(), "testPresenceDb");
+    }
+
+    public void testInsert() {
+
+        Presence p = PresenceFactory.createPresence("myUid1", new LatLng(30.1, 40.1), "my label", "my snippit", "my map");
+
+        _dbHelper.insert(p);
+    }
+
+    public void testBadInsert() {
+
+        Presence p = PresenceFactory.createPresence("myUid2", new LatLng(30.1, 40.1), "my label", "my snippit", "my map");
+
+        _dbHelper.insert(p);
+
+        boolean success = false;
+        try {
+            _dbHelper.insert(p); //should fail as dupe
+        } catch (SQLException ex) {
+            success = true;
+        }
+        assertTrue("double insert", success);
+    }
+
+    public void testReplace() {
+
+        Presence p = PresenceFactory.createPresence("myUid3", new LatLng(30.1, 40.1), "my label", "my snippit", "my map");
+        _dbHelper.replace(p);
+
+        boolean success = false;
+        try {
+            _dbHelper.insert(p);
+        } catch (SQLException ex) {
+            success = true;
+        }
+        assertTrue("should have been a dupe", success);
+
+        success = false;
+        _dbHelper.replace(p); //should work now too
+    }
+
+    public void testGet() throws JSONException {
+
+        Presence p = _dbHelper.get("myUid4", "my map");
+        assertNull("should not have found this presence", p);
+
+        Presence p1 = PresenceFactory.createPresence("myUid4", new LatLng(30.1, 40.1), "my label", "my snippit", "my map");
+        _dbHelper.insert(p1);
+
+        p = _dbHelper.get("myUid4", "my map");
+        assertNotNull("should have found this presence", p);
+        assertEquals("should have matching 'my label': " + p.toString(), p.getLabel(), "my label");
+        assertEquals("should have matching 'my snippit': " + p.toString(), p.getSnippet(), "my snippit");
+
+        Presence p2 = PresenceFactory.createPresence("myUid4", new LatLng(30.1, 40.1), "my new label", "my new snippit", "my map");
+        _dbHelper.replace(p2);
+        p = _dbHelper.get("myUid4", "my map");
+        assertNotNull("should have found this presence", p);
+        assertEquals("should have matching 'my new label': " + p.toString(), p.getLabel(), "my new label");
+        assertEquals("should have matching 'my new snippit': " + p.toString(), p.getSnippet(), "my new snippit");
+    }
+
+    public void testGetAll() throws JSONException {
+
+        Presence p = PresenceFactory.createPresence("myUid1", new LatLng(30.1, 40.1), "my label", "my snippit", "my new map");
+        _dbHelper.replace(p);
+        p = PresenceFactory.createPresence("myUid2", new LatLng(30.1, 40.1), "my label", "my snippit", "my new map");
+        _dbHelper.replace(p);
+        p = PresenceFactory.createPresence("myUid3", new LatLng(30.1, 40.1), "my label", "my snippit", "my new map");
+        _dbHelper.replace(p);
+        p = PresenceFactory.createPresence("myUid4", new LatLng(30.1, 40.1), "my label", "my snippit", "my new map");
+        _dbHelper.replace(p);
+
+        List<Presence> l = _dbHelper.getAll("my new map");
+        assertNotNull("should have found these", l);
+        assertEquals("wrong number of getAll(map) results: " + l, 4, l.size());
+    }
+
+    public void testDelete() throws JSONException {
+
+        Presence p1 = PresenceFactory.createPresence("myUid9", new LatLng(30.1, 40.1), "my newest label", "my snippit", "my newest map");
+        _dbHelper.replace(p1);
+
+        Presence p = _dbHelper.get("myUid9", "my newest map");
+        assertNotNull("should have found this", p);
+        assertEquals("should have matching 'my newest label': " + p.toString(), p.getLabel(), "my newest label");
+
+        _dbHelper.delete(p1);
+        p = _dbHelper.get("myUid9", "my newest map");
+        assertNull("should not have found this", p);
+    }
+}
+
---
+++ android/OeMap/src/main/java/com/onextent/oemap/presence/PresenceDbHelper.java
@@ -0,0 +1,139 @@
+package com.onextent.oemap.presence;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.SQLException;
+
+import com.onextent.android.util.OeLog;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PresenceDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "oemap_presences.db";
+    private static int DATABASE_VERSION = 1;
+    SQLiteDatabase _db;
+
+    public PresenceDbHelper(Context context, String dbname) { //for tests
+        super(context, dbname, null, DATABASE_VERSION);
+        establishDb();
+    }
+
+    public PresenceDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        establishDb();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        createTable(sqLiteDatabase);
+    }
+
+    private void createTable(SQLiteDatabase sqLiteDatabase) {
+
+        String qs = "CREATE TABLE presences (uid, spacename, data, PRIMARY KEY (uid, spacename));";
+        sqLiteDatabase.execSQL(qs);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS presences;");
+
+    }
+
+    private void establishDb() {
+
+        if (_db == null)
+            _db = getWritableDatabase();
+    }
+
+    public void cleanup() {
+        if (_db != null) {
+            _db.close();
+            _db = null;
+        }
+    }
+
+    public void insert(Presence presence) {
+        ContentValues values = new ContentValues();
+        values.put("uid", presence.getUID());
+        values.put("spacename", presence.getSpaceName());
+        values.put("data", presence.toString());
+        _db.insertOrThrow("presences", null, values);
+    }
+
+    public void replace(Presence presence) {
+        ContentValues values = new ContentValues();
+        values.put("uid", presence.getUID());
+        values.put("spacename", presence.getSpaceName());
+        values.put("data", presence.toString());
+        _db.replaceOrThrow("presences", null, values);
+    }
+
+    public void delete(Presence presence) {
+        _db.delete("presences", "uid='" + presence.getUID() + "' AND spacename='" + presence.getSpaceName() + "'", null);
+    }
+
+    public void deleteSpaceName(String spacename) {
+        _db.delete("presences", "spacename='" + spacename + "'", null);
+    }
+
+    public Presence get(String uid, String spacename) throws JSONException {
+        Cursor c = null;
+        Presence p = null;
+        //String[] cols = {"_id", "uid", "spacename", "data"};
+        String[] cols = {"uid", "spacename", "data"};
+        try {
+
+            c = _db.query(true, "presences", cols, "uid='" + uid + "' AND spacename='" + spacename + "'", null, null, null, null, null);
+            int DATA_FLD = c.getColumnIndex("data");
+            if (c.getCount() > 0) {
+                c.moveToFirst();
+                String json = c.getString(DATA_FLD);
+                p = PresenceFactory.createPresence(json);
+            }
+        } catch (SQLException ex) {
+            OeLog.e(ex.toString(), ex);
+        } finally {
+            if (c != null && !c.isClosed())
+                c.close();
+        }
+        return p;
+    }
+
+    public List<Presence> getAll(String spacename) throws JSONException {
+        Cursor c = null;
+        List<Presence> l = null;
+        String[] cols = {"uid", "spacename", "data"};
+        try {
+            c = _db.query(true, "presences", cols, "spacename='" + spacename + "'", null, null, null, null, null);
+            int DATA_FLD = c.getColumnIndex("data");
+            int numRows = c.getCount();
+            c.moveToFirst();
+            for (int i = 0; i < numRows; i++) {
+                if (l == null)
+                    l = new ArrayList<Presence>();
+                String json = c.getString(DATA_FLD);
+                Presence p = PresenceFactory.createPresence(json);
+                l.add(p);
+                c.moveToNext();
+            }
+        } catch (SQLException ex) {
+            OeLog.e(ex.toString(), ex);
+        } finally {
+            if (c != null && !c.isClosed())
+                c.close();
+        }
+        return l;
+    }
+}
+
