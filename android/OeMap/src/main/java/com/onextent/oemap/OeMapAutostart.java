752879c83387a729d30b710aa1a9038f2891ad14
Ed Sweeney <ed@onextent.com>
2013-08-04 11:27:26 -0700

    presence service stubbed out with notification in taskbar

---
+++ android/OeMap/src/main/java/com/onextent/oemap/OeMapAutostart.java
@@ -0,0 +1,29 @@
+package com.onextent.oemap;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.onextent.android.util.OeLog;
+import com.onextent.oemap.presence.OeMapPresenceService;
+
+import android.net.Uri;
+
+public class OeMapAutostart extends BroadcastReceiver
+{
+    public void onReceive(Context context, Intent intent)
+    {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+
+            Intent i =  new Intent(context, OeMapPresenceService.class);
+            i.putExtra("reason", "boot");
+            context.startService(i);
+
+        } else {
+
+            OeLog.w("unexpected intent: " + intent);
+        }
+    }
+}
+
