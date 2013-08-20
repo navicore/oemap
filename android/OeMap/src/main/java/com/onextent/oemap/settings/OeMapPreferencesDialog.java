790bedef452ac037b5594b4da0dcfaf94b5ec5dd
Ed Sweeney <ed@onextent.com>
2013-08-20 11:12:53 -0700

    no more done button, long press for list

--- android/OeMap/src/main/java/com/onextent/oemap/settings/OeMapPreferencesDialog.java
+++ android/OeMap/src/main/java/com/onextent/oemap/settings/OeMapPreferencesDialog.java
@@ -3,13 +3,15 @@ package com.onextent.oemap.settings;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.inputmethod.EditorInfo;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.onextent.android.util.OeLog;
 import com.onextent.oemap.OeMapActivity;
@@ -40,7 +42,6 @@ public class OeMapPreferencesDialog extends DialogFragment {
             snippitEdit.setText(snippit);
 
             setShowZoomCtlCb();
-            setDoneButtonCb();
 
         } catch (Exception e) {
             OeLog.e(e.toString(), e);
@@ -49,31 +50,22 @@ public class OeMapPreferencesDialog extends DialogFragment {
         return _view;
     }
 
-    private void setDoneButtonCb() {
+    @Override
+    public void onPause() {
+        EditText edit = (EditText) _view.findViewById(R.id.pref_username);
+        String s = edit.getText().toString();
+        _prefs.replace(getString(R.string.pref_username), s);
 
-        Button doneButton = (Button) _view.findViewById(R.id.pref_done_button);
-        doneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveSettings();
-                dismiss();
-            }
-        });
-    }
+        edit = (EditText) _view.findViewById(R.id.pref_snippit);
+        s = edit.getText().toString();
+        _prefs.replace(getString(R.string.pref_snippit), s);
 
-    private void saveSettings() {
-        EditText labelEdit = (EditText) _view.findViewById(R.id.pref_username);
-        String label = labelEdit.getText().toString();
-        _prefs.replace(getString(R.string.pref_username), label);
-
-        EditText snippitEdit = (EditText) _view.findViewById(R.id.pref_snippit);
-        String snippit = snippitEdit.getText().toString();
-        _prefs.replace(getString(R.string.pref_snippit), snippit);
+        super.onPause();
     }
 
     private void setShowZoomCtlCb() {
 
-        boolean zoom = _prefs.getBoolean(getString(R.string.pref_show_zoom_ctl), false);
+        boolean zoom = _prefs.getBoolean(getString(R.string.pref_show_zoom_ctl), true);
         CheckBox zoomCheckBox = (CheckBox) _view.findViewById(R.id.pref_show_zoom_control);
         zoomCheckBox.setChecked(zoom);
 
