8824b587122bf0933ad2abb3628bff8181cfc1ab
Ed Sweeney <ed@onextent.com>
2013-08-20 22:38:42 -0700

    fix title bug and set camera when marker searched

---
+++ android/OeMap/src/main/java/com/onextent/oemap/presence/SearchDialog.java
@@ -0,0 +1,38 @@
+package com.onextent.oemap.presence;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import com.onextent.android.util.OeLog;
+import com.onextent.oemap.OeMapActivity;
+import com.onextent.oemap.R;
+import com.onextent.oemap.provider.KvHelper;
+
+public class SearchDialog extends DialogFragment {
+
+    private View _view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Dialog d = getDialog();
+        if (d != null) d.setTitle(getString(R.string.app_name) + " Search");
+        _view = inflater.inflate(R.layout.search_dialog_layout, container, false);
+        try {
+
+
+        } catch (Exception e) {
+            OeLog.e(e.toString(), e);
+        }
+
+        return _view;
+    }
+}
+
