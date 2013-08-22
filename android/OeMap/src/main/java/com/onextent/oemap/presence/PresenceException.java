19200ed20eb7492f0996fc4fb2d3e4756810b98c
Ed Sweeney <ed@onextent.com>
2013-08-22 08:40:09 -0700

    new menus

---
+++ android/OeMap/src/main/java/com/onextent/oemap/presence/PresenceException.java
@@ -0,0 +1,14 @@
+package com.onextent.oemap.presence;
+
+public class PresenceException extends Exception {
+
+    public PresenceException(String msg) {
+        super(msg);
+    }
+    public PresenceException(String msg, Throwable err) {
+        super(msg, err);
+    }
+    public PresenceException(Throwable err) {
+        super(err);
+    }
+}
