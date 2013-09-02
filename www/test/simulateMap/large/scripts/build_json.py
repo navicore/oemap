dbea17a055ae8d1e96bcb65b2a8ab6f3e51f6e94
Ed Sweeney <ed@onextent.com>
2013-09-02 07:48:23 -0700

    python direct load zulu time

--- www/test/simulateMap/large/scripts/build_json.py
+++ www/test/simulateMap/large/scripts/build_json.py
@@ -2,27 +2,40 @@
 import os
 import json
 import datetime
-
-filedir = os.path.dirname(os.path.realpath(__file__))
-
-import time
+import pytz
 from itertools import izip
 
-snippit = 'good afternoon'
-space = 'big test'
-ttl = 2
+MY_DIR = os.path.dirname(os.path.realpath(__file__))
+NAMES_FILE = MY_DIR + '/../tmp/names.txt'
+POINTS_FILE = MY_DIR + '/../tmp/points.txt'
 
-with open(filedir + '/../tmp/names.txt') as namefile, open(filedir + '/../tmp/points.txt') as pointfile:
+
+SNIPPIT = 'good morning'
+SPACE = 'big test'
+TTL = 2
+
+def utc_now():
+    return datetime.datetime.now(
+            pytz.timezone('UTC')).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
+
+# pylint: disable=C0321
+with open(NAMES_FILE) as namefile, open(POINTS_FILE) as pointfile:
+# pylint: enable=C0321
     
+    # pylint: disable=C0103
     counter = 0
+    # pylint: enable=C0103
     for name, c in izip(namefile, pointfile):
 
         counter += 1
         name = name.strip()
         c = c.strip()
 
-        if (name == None): continue
-        if (c == None): continue
+        if (name == None): 
+            continue
+        if (c == None): 
+            continue
         coord = c.split(',')
         coord = [float(coord[0]), float(coord[1])]
         lat = coord[0]
@@ -31,18 +44,20 @@ with open(filedir + '/../tmp/names.txt') as namefile, open(filedir + '/../tmp/po
         presence = {}
         presence['uid'] = str(counter) + '_1234567890_TEST'
         presence['label'] = name
-        presence['snippit'] = snippit
-        presence['ttl'] = ttl
-        now = str(datetime.datetime.now())
+        presence['snippit'] = SNIPPIT
+        presence['ttl'] = TTL
+        
+        now = utc_now()
+
         presence['time'] = now
-        if ttl == 1:
+        if TTL == 1:
             ttl_idx = 'short_ttl_start_time'
-        elif ttl == 2:
+        elif TTL == 2:
             ttl_idx = 'medium_ttl_start_time'
         else:
             ttl_idx = 'long_ttl_start_time'
         presence[ttl_idx] = now
-        presence['space'] = space
+        presence['space'] = SPACE
         presence['location'] = {}
         presence['location']['type'] = "Point"
         presence['location']['coordinates'] = [lon, lat]
